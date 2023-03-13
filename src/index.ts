9de436069bc80a3a50f9c7f20d2dc9bb153e1181
宋彦龙 <songyanlong@51k1k.com>
2023-03-13 15:54:54 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ interface trurnUrlParams {
   [key: string]: string;
 }
 
-type getUrlParamsType = (url?: string) => trurnUrlParams | undefined;
 /**
  * 获取url地址中的参数
  * @param url url地址
  * @returns
  */
 
-export const getUrlParams: getUrlParamsType = (url) => {
+export const getUrlParams: (url?: string) => trurnUrlParams | undefined = (url) => {
   const URL = url ?? location.href;
   let params: trurnUrlParams = {};
   if (!URL || typeof URL !== "string")
